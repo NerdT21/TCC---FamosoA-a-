18437a6e15ec000fe5c47ad9efc32ef349f4d54c
João Vitor <joaof-neiva@hotmail.com>
2018-10-12 19:28:01 -0300

    <<comantado alguns business>>

--- FamosoAça/Classes/FolhaPagamento/FolhaPagamentoBusiness.cs
+++ FamosoAça/Classes/FolhaPagamento/FolhaPagamentoBusiness.cs
@@ -15,31 +15,31 @@ namespace FamosoAça.Classes.FolhaPagamento
             return id;
         }
 
-        public void Altera(FolhaPagamentoDTO dto)
-        {
-            FolhaPagamentoDataBase db = new FolhaPagamentoDataBase();
-            db.Alterar(dto);
-        }
+        //public void Altera(FolhaPagamentoDTO dto)
+        //{
+        //    FolhaPagamentoDataBase db = new FolhaPagamentoDataBase();
+        //    db.Alterar(dto);
+        //}
 
-        public void Remover(int id)
-        {
-            FolhaPagamentoDataBase db= new FolhaPagamentoDataBase();
-            db.Remover(id);
-        }
+        //public void Remover(int id)
+        //{
+        //    FolhaPagamentoDataBase db= new FolhaPagamentoDataBase();
+        //    db.Remover(id);
+        //}
 
        
-        public List<FolhaPagamentoDTO> Listar()
-        {
-            FolhaPagamentoDataBase db = new FolhaPagamentoDataBase();
-            List<FolhaPagamentoDTO> list = db.Listar();
-            return list;
-        }
+        //public List<FolhaPagamentoDTO> Listar()
+        //{
+        //    FolhaPagamentoDataBase db = new FolhaPagamentoDataBase();
+        //    List<FolhaPagamentoDTO> list = db.Listar();
+        //    return list;
+        //}
         
-        public List<FolhaPagamentoDTO> Consultar(string consult)
-        {
-            FolhaPagamentoDataBase db = new FolhaPagamentoDataBase();
-            return db.Consultar(consult);
-        }
+        //public List<FolhaPagamentoDTO> Consultar(string consult)
+        //{
+        //    FolhaPagamentoDataBase db = new FolhaPagamentoDataBase();
+        //    return db.Consultar(consult);
+        //}
 
        
  
--- FamosoAça/Classes/ImpostoRenda/ImpostoRendaBusiness.cs
+++ FamosoAça/Classes/ImpostoRenda/ImpostoRendaBusiness.cs
@@ -8,38 +8,38 @@ namespace FamosoAça.Classes.ImpostoRenda
 {
     public class ImpostoRendaBusiness
     {
-         public int Salvar(ImpostoRendaDTO dto)
-        {
-            ImpostoRendaDataBase db = new ImpostoRendaDataBase();
-            int id = db.Salvar(dto);
-            return id;
-        }
+        // public int Salvar(ImpostoRendaDTO dto)
+        //{
+        //    ImpostoRendaDataBase db = new ImpostoRendaDataBase();
+        //    int id = db.Salvar(dto);
+        //    return id;
+        //}
 
-        public void Altera(ImpostoRendaDTO dto)
-        {
-            ImpostoRendaDataBase db = new ImpostoRendaDataBase();
-            db.Alterar(dto);
-        }
+        //public void Altera(ImpostoRendaDTO dto)
+        //{
+        //    ImpostoRendaDataBase db = new ImpostoRendaDataBase();
+        //    db.Alterar(dto);
+        //}
 
-        public void Remover(int id)
-        {
-            ImpostoRendaDataBase db= new ImpostoRendaDataBase();
-            db.Remover(id);
-        }
+        //public void Remover(int id)
+        //{
+        //    ImpostoRendaDataBase db= new ImpostoRendaDataBase();
+        //    db.Remover(id);
+        //}
 
        
-        public List<ImpostoRendaDTO> Listar()
-        {
-            ImpostoRendaDataBase db = new ImpostoRendaDataBase();
-            List<ImpostoRendaDTO> list = db.Listar();
-            return list;
-        }
+        //public List<ImpostoRendaDTO> Listar()
+        //{
+        //    ImpostoRendaDataBase db = new ImpostoRendaDataBase();
+        //    List<ImpostoRendaDTO> list = db.Listar();
+        //    return list;
+        //}
         
-        public List<ImpostoRendaDTO> Consultar(string consult)
-        {
-            ImpostoRendaDataBase db = new ImpostoRendaDataBase();
-            return db.Consultar(consult);
-        }
+        //public List<ImpostoRendaDTO> Consultar(string consult)
+        //{
+        //    ImpostoRendaDataBase db = new ImpostoRendaDataBase();
+        //    return db.Consultar(consult);
+        //}
 
        
  
