d16630075dcf2f34e7da195fb8e58774696116d7
infod32 <infod32@INFO14>
2018-10-19 17:28:23 -0300

    hehe

---
+++ FamosoAça/Screens/Entregavel III/Controle De Venda/frmCadastrarVenda.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace FamosoAça.Screens.Entregavel_III.Constrole_De_Venda
+{
+    partial class frmCadastrarVenda
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbldata = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudPreco = new System.Windows.Forms.NumericUpDown();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtdata = new System.Windows.Forms.MaskedTextBox();
+            this.cboproduto = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPreco)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbldata
+            // 
+            this.lbldata.AutoSize = true;
+            this.lbldata.BackColor = System.Drawing.Color.Transparent;
+            this.lbldata.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldata.ForeColor = System.Drawing.Color.Black;
+            this.lbldata.Location = new System.Drawing.Point(201, 138);
+            this.lbldata.Name = "lbldata";
+            this.lbldata.Size = new System.Drawing.Size(51, 25);
+            this.lbldata.TabIndex = 71;
+            this.lbldata.Text = "Data";
+            this.lbldata.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(120, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 25);
+            this.label3.TabIndex = 76;
+            this.label3.Text = "Preço do Produto";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // nudPreco
+            // 
+            this.nudPreco.DecimalPlaces = 2;
+            this.nudPreco.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudPreco.Location = new System.Drawing.Point(291, 222);
+            this.nudPreco.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.nudPreco.Name = "nudPreco";
+            this.nudPreco.Size = new System.Drawing.Size(73, 20);
+            this.nudPreco.TabIndex = 73;
+            this.nudPreco.ThousandsSeparator = true;
+            this.nudPreco.ValueChanged += new System.EventHandler(this.nudPreco_ValueChanged);
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCadastrar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(250, 297);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(125, 47);
+            this.btnCadastrar.TabIndex = 74;
+            this.btnCadastrar.Text = "Confirmar";
+            this.btnCadastrar.UseVisualStyleBackColor = false;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(172, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 25);
+            this.label4.TabIndex = 78;
+            this.label4.Text = "Produto";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtdata
+            // 
+            this.txtdata.Location = new System.Drawing.Point(292, 142);
+            this.txtdata.Mask = "00/00/0000";
+            this.txtdata.Name = "txtdata";
+            this.txtdata.Size = new System.Drawing.Size(72, 20);
+            this.txtdata.TabIndex = 79;
+            this.txtdata.ValidatingType = typeof(System.DateTime);
+            // 
+            // cboproduto
+            // 
+            this.cboproduto.FormattingEnabled = true;
+            this.cboproduto.Location = new System.Drawing.Point(292, 69);
+            this.cboproduto.Name = "cboproduto";
+            this.cboproduto.Size = new System.Drawing.Size(121, 21);
+            this.cboproduto.TabIndex = 80;
+            // 
+            // frmCadastrarVenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cboproduto);
+            this.Controls.Add(this.txtdata);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.nudPreco);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbldata);
+            this.Name = "frmCadastrarVenda";
+            this.Size = new System.Drawing.Size(596, 397);
+            this.Load += new System.EventHandler(this.frmVenda_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudPreco)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbldata;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudPreco;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox txtdata;
+        private System.Windows.Forms.ComboBox cboproduto;
+    }
+}
---
+++ FamosoAça/Screens/Entregavel III/Controle De Venda/frmCadastrarVenda.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FamosoAça.Screens.Entregavel_III.Constrole_De_Venda
+{
+    public partial class frmCadastrarVenda : UserControl
+    {
+        public frmCadastrarVenda()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDesc_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nudPreco_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMarca_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmVenda_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
