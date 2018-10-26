f0269ae2f17a0b76d768e5c5ceacc8c7f968e44b
infod32 <infod32@INFO14>
2018-10-26 18:41:19 -0200

    ((CRUID do gastos feita))

---
+++ FamosoAça/Classes/Gastos/GastosBussines.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamosoAça.Classes.Gastos
+{
+   public class GastosBussines
+    {
+        public int Salvar(GastosDTO dto)
+        {
+            GastosDatabase db = new GastosDatabase();
+            int id = db.Salvar(dto);
+            return id;
+        }
+
+        public void Altera(GastosDTO dto)
+        {
+            GastosDatabase db = new GastosDatabase();
+            db.Alterar(dto);
+        }
+
+        public void Remover(int id)
+        {
+            GastosDatabase db = new GastosDatabase();
+            db.Remover(id);
+        }
+
+
+        public List<GastosDTO> Listar()
+        {
+            GastosDatabase db = new GastosDatabase();
+            List<GastosDTO> list = db.Listar();
+            return list;
+        }
+
+        public List<GastosDTO> Consultar(string nome)
+        {
+            GastosDatabase db = new GastosDatabase();
+            return db.Consultar(nome);
+        }
+    }
+}
---
+++ FamosoAça/Classes/Gastos/GastosDatabase.cs
@@ -0,0 +1,103 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamosoAça.Classes.Gastos
+{
+  public class GastosDatabase
+    {
+        public int Salvar(GastosDTO dto)
+        {
+            string script = @"INSERT INTO tb_gastosAdicionais(
+                                          nm_gasto,
+	                                      ds_gasto,
+	                                      vl_gasto)
+                                    VALUES(@nm_gasto,
+	                                       @ds_gasto,
+	                                       @vl_gasto)";
+            List<MySqlParameter> parms = new List<MySqlParameter>();
+            parms.Add(new MySqlParameter("nm_gasto", dto.Nome));
+            parms.Add(new MySqlParameter("ds_gasto", dto.Descrição));
+            parms.Add(new MySqlParameter("vl_gasto", dto.Valor));
+
+            Database db = new Database();
+            int pk = db.ExecuteInsertScriptWithPk(script, parms);
+            return pk;
+
+        }
+        public void Alterar(GastosDTO dto)
+        {
+            string script = @"UPDATE tb_gastosAdicionais SET nm_gasto = @nm_gasto,
+                                                             ds_gasto = @ds_gasto,
+                                                             vl_gasto = @vl_gasto WHERE
+                                                             id_gasto = @id_gasto";
+            List<MySqlParameter> parms = new List<MySqlParameter>();
+            parms.Add(new MySqlParameter("id_gasto", dto.Id));
+            parms.Add(new MySqlParameter("nm_gasto", dto.Nome));
+            parms.Add(new MySqlParameter("ds_gasto", dto.Descrição));
+            parms.Add(new MySqlParameter("vl_gasto", dto.Valor));
+
+            Database db = new Database();
+            db.ExecuteInsertScript(script, parms);
+
+        }
+        public void Remover(int idgasto)
+        {
+            string script = @"DELETE FROM tb_gastosAdicionais WHERE id_gasto = @id_gasot";
+
+            List<MySqlParameter> parms = new List<MySqlParameter>();
+
+            Database db = new Database();
+            db.ExecuteInsertScript(script, parms);
+        }
+        public List<GastosDTO> Listar()
+        {
+            string script = @"SELECT *FROM tb_gastosAdicionais";
+
+            List<MySqlParameter> parms = new List<MySqlParameter>();
+
+            Database db = new Database();
+            MySqlDataReader reader = db.ExecuteSelectScript(script, parms);
+
+            List<GastosDTO> Gastos = new List<GastosDTO>();
+            while (reader.Read())
+            {
+                GastosDTO dto = new GastosDTO();
+                dto.Id = reader.GetInt32("id_gasto");
+                dto.Nome = reader.GetString("nm_gasto");
+                dto.Descrição = reader.GetString("ds_gasto");
+                dto.Valor = reader.GetDecimal("vl_valor");
+
+                Gastos.Add(dto);
+            }
+            reader.Close();
+            return Gastos;
+        }
+        public List<GastosDTO> Consultar(string nome)
+        {
+            string script = @"SELECT * FROM tb_gastosAdicionais WHERE nm_gasto LIKE @nm_gasto";
+            List<MySqlParameter> parms = new List<MySqlParameter>();
+            parms.Add(new MySqlParameter("nm_gasto", nome + "%"));
+
+            Database db = new Database();
+
+            MySqlDataReader reader = db.ExecuteSelectScript(script, parms);
+            List<GastosDTO> Gastos = new List<GastosDTO>();
+            while (reader.Read())
+            {
+                GastosDTO dto = new GastosDTO();
+                dto.Id = reader.GetInt32("id_gasto");
+                dto.Nome = reader.GetString("nm_gasto");
+                dto.Descrição = reader.GetString("ds_gasto");
+                dto.Valor = reader.GetDecimal("vl_valor");
+
+                Gastos.Add(dto);
+            }
+            reader.Close();
+            return Gastos;
+        }
+    }
+}
