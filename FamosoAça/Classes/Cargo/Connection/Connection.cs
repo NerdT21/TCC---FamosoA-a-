cbb743f735ecd0e446d1ef4860301b4229d53990
infod32 <infod32@INFO19>
2018-11-13 13:45:58 -0200

    {{Banco  local}}

--- FamosoAça/Classes/Cargo/Connection/Connection.cs
+++ FamosoAça/Classes/Cargo/Connection/Connection.cs
@@ -12,9 +12,9 @@ namespace FamosoAça.Classes
     {
         public MySqlConnection Create()
         {
-            string connectionString = "server=70.37.57.127; database=FamosoAcaiDB; uid=nsf; password=nsf@2018; SslMode=none";
+            //string connectionString = "server=70.37.57.127; database=FamosoAcaiDB; uid=nsf; password=nsf@2018; SslMode=none";
             //string connectionString = "server=192.168.0.100; database=FamosoAcaiDB; uid=root; password=; SslMode=none";
-            // string connectionString = "server=localhost; database=FamosoAcaiDB; uid=root; password=; SslMode=none";
+            string connectionString = "server=localhost; database=FamosoAcaiDB; uid=root; password=; SslMode=none";
 
             MySqlConnection connection = new MySqlConnection(connectionString);
             connection.Open();
